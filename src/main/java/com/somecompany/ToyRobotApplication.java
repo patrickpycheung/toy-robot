86d78d16f2b1d923929ef2242bbc08220a5a3a45
Patrick Cheung <patrick_cheung23@hotmail.com>
2021-03-08 12:03:33 +1100

    Add handling of file input before manual input on console begins.

--- src/main/java/com/somecompany/ToyRobotApplication.java
+++ src/main/java/com/somecompany/ToyRobotApplication.java
@@ -1,8 +1,12 @@
 package com.somecompany;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,11 +34,59 @@ public class ToyRobotApplication implements CommandLineRunner {
 		SpringApplication.run(ToyRobotApplication.class, args);
 	}
 
+	@Value("${inputFile.Path}")
+	private String inputFilePath;
+
+	@Value("${inputFile.Renamed.folder}")
+	private String inputFileRenamedFolder;
+
+	@Value("${inputFile.Renamed.suffix}")
+	private String inputFileRenamedSuffix;
+
 	@Value("${errorMsg.ioException}")
 	private String ERROR_MSG_IO_EXCEPTION;
 
 	@Override
 	public void run(String... args) {
+
+		/* Handle file input */
+
+		System.out.println("Begin handling file input...");
+
+		try {
+			File inputFile = new File(inputFilePath);
+
+			BufferedReader fileBR = new BufferedReader(new FileReader(inputFile));
+
+			try {
+				String usrInput;
+				while ((usrInput = fileBR.readLine()) != null) {
+					// Handle one line of command
+					handleUserInput(usrInput);
+				}
+
+				// Rename file after processing
+
+				String inputFileName = inputFile.getName();
+				Instant instant = Instant.now();
+
+				// Append current timestamp to file, and move to processed folder
+				File renamedFile = new File(inputFileRenamedFolder + inputFileName + inputFileRenamedSuffix + instant);
+
+				inputFile.renameTo(renamedFile);
+			} catch (IOException e) {
+
+				System.out.println(ERROR_MSG_IO_EXCEPTION);
+				log.error(ERROR_MSG_IO_EXCEPTION);
+			}
+		} catch (FileNotFoundException e) {
+			// No file input, just proceed
+		}
+
+		System.out.println("Finished handling file input.");
+
+		/* Handle manual input on console */
+
 		// Instruct user to perform input
 		System.out.println("Welcome to toy robot application!");
 		System.out.println("Below are the possible operations:");
@@ -94,6 +146,47 @@ public class ToyRobotApplication implements CommandLineRunner {
 				System.out.println(ERROR_MSG_IO_EXCEPTION);
 				log.error(ERROR_MSG_IO_EXCEPTION);
 			}
+		}
+	}
+
+	private void handleUserInput(String usrInput) {
+		try {
+			// Validate user input
+			validationService.validateUserInput(usrInput);
+
+			String[] usrInputArr = usrInput.split(" ");
+
+			String command = usrInputArr[0].toUpperCase();
+
+			if (command.equals(Command.PLACE.name())) {
+				// PLACE command
+
+				String[] placeParamArr = usrInputArr[1].split(",");
+
+				String XCor = placeParamArr[0];
+				String YCor = placeParamArr[1];
+				String facing = placeParamArr[2];
+
+				toyRobotService.place(XCor, YCor, facing);
+			} else if (command.equals(Command.MOVE.name())) {
+				// MOVE command
+
+				toyRobotService.move();
+			} else if (command.equals(Command.LEFT.name())) {
+				// LEFT command
+
+				toyRobotService.left();
+			} else if (command.equals(Command.RIGHT.name())) {
+				// RIGHT command
+
+				toyRobotService.right();
+			} else if (command.equals(Command.REPORT.name())) {
+				// REPORT command
+
+				System.out.println(toyRobotService.report());
+			}
+		} catch (IllegalArgumentException exception) {
+			System.out.println(exception.getMessage());
 		}
 	}
 }
