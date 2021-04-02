5328473ba7a1bda1104ad4acc5c86388b6da015a
Patrick Cheung <patrick_cheung23@hotmail.com>
2021-04-02 14:56:13 +1100

    Add support for calling left function from API call.

--- src/main/java/com/somecompany/controller/ToyRobotController.java
+++ src/main/java/com/somecompany/controller/ToyRobotController.java
@@ -1,92 +1,101 @@
 package com.somecompany.controller;
 
+import com.somecompany.model.Location;
+import com.somecompany.service.ToyRobotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.somecompany.model.Location;
-import com.somecompany.service.ToyRobotService;
-
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/toyrobot")
 public class ToyRobotController {
 
-	@Autowired
-	private ToyRobotService toyRobotService;
+    @Autowired
+    private ToyRobotService toyRobotService;
 
-	@Value("${errorMsg.invalidApiParams}")
-	private String ERROR_INVALID_API_PARAMS;
+    @Value("${errorMsg.invalidApiParams}")
+    private String ERROR_INVALID_API_PARAMS;
 
-	/**
-	 * API endpoint for "REPORT" function.
-	 * 
-	 * @return ResponseEntity<String>
-	 */
-	@GetMapping("/report")
-	public ResponseEntity<Mono<String>> report() {
+    /**
+     * API endpoint for "REPORT" function.
+     *
+     * @return ResponseEntity<String>
+     */
+    @GetMapping("/report")
+    public ResponseEntity<Mono<String>> report() {
 
-		try {
-			return ResponseEntity.status(HttpStatus.OK).body(Mono.just(toyRobotService.report()));
-		} catch (IllegalArgumentException exception) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Mono.just(exception.getMessage()));
-		}
-	}
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(Mono.just(toyRobotService.report()));
+        } catch (IllegalArgumentException exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Mono.just(exception.getMessage()));
+        }
+    }
 
-	/**
-	 * API endpoint for "PLACE" function.
-	 * 
-	 * @param location
-	 * @return ResponseEntity<String>
-	 */
-	@PutMapping("/place")
-	public ResponseEntity<Mono<String>> place(@RequestBody Location location) {
+    /**
+     * API endpoint for "PLACE" function.
+     *
+     * @param location
+     * @return ResponseEntity<String>
+     */
+    @PutMapping("/place")
+    public ResponseEntity<Mono<String>> place(@RequestBody Location location) {
 
-		try {
-			toyRobotService.place(String.valueOf(location.getXCor()), String.valueOf(location.getYCor()),
-					location.getFacing().name());
-			return ResponseEntity.status(HttpStatus.OK).body(Mono.just("Successfully placed robot on grid."));
-		} catch (IllegalArgumentException exception) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Mono.just(exception.getMessage()));
-		}
+        try {
+            toyRobotService.place(String.valueOf(location.getXCor()), String.valueOf(location.getYCor()),
+                    location.getFacing().name());
+            return ResponseEntity.status(HttpStatus.OK).body(Mono.just("Successfully placed robot on grid."));
+        } catch (IllegalArgumentException exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Mono.just(exception.getMessage()));
+        }
 
-	}
+    }
 
-	/**
-	 * API endpoint for "MOVE" function.
-	 * 
-	 * @return ResponseEntity<Mono<String>>
-	 */
-	@PostMapping("/move")
-	public ResponseEntity<Mono<String>> move() {
+    /**
+     * API endpoint for "MOVE" function.
+     *
+     * @return ResponseEntity<Mono < String>>
+     */
+    @PostMapping("/move")
+    public ResponseEntity<Mono<String>> move() {
 
-		try {
-			toyRobotService.move();
-			return ResponseEntity.status(HttpStatus.OK)
-					.body(Mono.just("Successfully moved robot, new location is " + toyRobotService.report()));
-		} catch (IllegalArgumentException exception) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Mono.just(exception.getMessage()));
-		}
-	}
+        try {
+            toyRobotService.move();
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(Mono.just("Successfully moved robot, new location is " + toyRobotService.report()));
+        } catch (IllegalArgumentException exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Mono.just(exception.getMessage()));
+        }
+    }
 
-	/**
-	 * Handle handleHttpMessageNotReadableException, e.g. cannot parse the parameters.
-	 * 
-	 * @return ResponseEntity<String>
-	 */
-	@ExceptionHandler(HttpMessageNotReadableException.class)
-	public ResponseEntity<String> handleHttpMessageNotReadableException() {
+    /**
+     * API endpoint for "LEFT" function.
+     *
+     * @return ResponseEntity<Mono < String>>
+     */
+    @PostMapping("/left")
+    public ResponseEntity<Mono<String>> left() {
 
-		return ResponseEntity.badRequest().body(ERROR_INVALID_API_PARAMS);
-	}
+        try {
+            toyRobotService.left();
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(Mono.just("Successfully turned robot to the left, new location is " + toyRobotService.report()));
+        } catch (IllegalArgumentException exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Mono.just(exception.getMessage()));
+        }
+    }
+
+    /**
+     * Handle handleHttpMessageNotReadableException, e.g. cannot parse the parameters.
+     *
+     * @return ResponseEntity<String>
+     */
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException() {
+
+        return ResponseEntity.badRequest().body(ERROR_INVALID_API_PARAMS);
+    }
 }
--- src/test/java/com/somecompany/ToyRobotLeftTest.java
+++ src/test/java/com/somecompany/ToyRobotLeftTest.java
@@ -1,132 +1,193 @@
 package com.somecompany;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import com.somecompany.model.Facing;
 import com.somecompany.model.Grid;
 import com.somecompany.model.Location;
 import com.somecompany.model.Robot;
 import com.somecompany.service.ToyRobotService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = ToyRobotApplication.class, initializers = ConfigDataApplicationContextInitializer.class)
-@ActiveProfiles("dev")
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class ToyRobotLeftTest {
 
-	@Autowired
-	private Robot robot;
+    @Autowired
+    private Robot robot;
 
-	@Autowired
-	private Grid grid;
+    @Autowired
+    private Grid grid;
 
-	@Autowired
-	private ToyRobotService toyRobotService;
+    @Autowired
+    private ToyRobotService toyRobotService;
 
-	@Value("${errorMsg.noLocation}")
-	private String ERROR_MSG_NO_LOCATION;
+    @Autowired
+    private WebTestClient webTestClient;
 
-	@BeforeEach
-	public void init() {
-		grid.setWidth(5);
-		grid.setHeight(5);
+    @Value("${errorMsg.noLocation}")
+    private String ERROR_MSG_NO_LOCATION;
 
-		robot.setLocation(null);
-	}
+    @BeforeEach
+    public void init() {
+        grid.setWidth(5);
+        grid.setHeight(5);
 
-	@Test
-	public void shouldBeAbleToFaceRobotNorthIfRobotIsFacingEast() {
+        robot.setLocation(null);
+    }
 
-		Location location = new Location();
-		location.setXCor(1);
-		location.setYCor(2);
-		location.setFacing(Facing.EAST);
+    @Test
+    public void shouldBeAbleToFaceRobotNorthIfRobotIsFacingEast() {
 
-		robot.setLocation(location);
+        Location location = new Location();
+        location.setXCor(1);
+        location.setYCor(2);
+        location.setFacing(Facing.EAST);
 
-		// Actual result
-		toyRobotService.left();
+        robot.setLocation(location);
 
-		// Assertion
-		assertEquals(1, robot.getLocation().getXCor());
-		assertEquals(2, robot.getLocation().getYCor());
-		assertEquals(Facing.NORTH, robot.getLocation().getFacing());
-	}
+        // Actual result
+        toyRobotService.left();
 
-	@Test
-	public void shouldBeAbleToFaceRobotEastIfRobotIsFacingSouth() {
+        // Assertion
+        assertEquals(1, robot.getLocation().getXCor());
+        assertEquals(2, robot.getLocation().getYCor());
+        assertEquals(Facing.NORTH, robot.getLocation().getFacing());
+    }
 
-		Location location = new Location();
-		location.setXCor(1);
-		location.setYCor(2);
-		location.setFacing(Facing.SOUTH);
+    @Test
+    public void shouldBeAbleToFaceRobotEastIfRobotIsFacingSouth() {
 
-		robot.setLocation(location);
+        Location location = new Location();
+        location.setXCor(1);
+        location.setYCor(2);
+        location.setFacing(Facing.SOUTH);
 
-		// Actual result
-		toyRobotService.left();
+        robot.setLocation(location);
 
-		// Assertion
-		assertEquals(1, robot.getLocation().getXCor());
-		assertEquals(2, robot.getLocation().getYCor());
-		assertEquals(Facing.EAST, robot.getLocation().getFacing());
-	}
+        // Actual result
+        toyRobotService.left();
 
-	@Test
-	public void shouldBeAbleToFaceRobotSouthIfRobotIsFacingWest() {
+        // Assertion
+        assertEquals(1, robot.getLocation().getXCor());
+        assertEquals(2, robot.getLocation().getYCor());
+        assertEquals(Facing.EAST, robot.getLocation().getFacing());
+    }
 
-		Location location = new Location();
-		location.setXCor(1);
-		location.setYCor(2);
-		location.setFacing(Facing.WEST);
+    @Test
+    public void shouldBeAbleToFaceRobotSouthIfRobotIsFacingWest() {
 
-		robot.setLocation(location);
+        Location location = new Location();
+        location.setXCor(1);
+        location.setYCor(2);
+        location.setFacing(Facing.WEST);
 
-		// Actual result
-		toyRobotService.left();
+        robot.setLocation(location);
 
-		// Assertion
-		assertEquals(1, robot.getLocation().getXCor());
-		assertEquals(2, robot.getLocation().getYCor());
-		assertEquals(Facing.SOUTH, robot.getLocation().getFacing());
-	}
+        // Actual result
+        toyRobotService.left();
 
-	@Test
-	public void shouldBeAbleToFaceRobotWestIfRobotIsFacingNorth() {
+        // Assertion
+        assertEquals(1, robot.getLocation().getXCor());
+        assertEquals(2, robot.getLocation().getYCor());
+        assertEquals(Facing.SOUTH, robot.getLocation().getFacing());
+    }
 
-		Location location = new Location();
-		location.setXCor(1);
-		location.setYCor(2);
-		location.setFacing(Facing.NORTH);
+    @Test
+    public void shouldBeAbleToFaceRobotWestIfRobotIsFacingNorth() {
 
-		robot.setLocation(location);
+        Location location = new Location();
+        location.setXCor(1);
+        location.setYCor(2);
+        location.setFacing(Facing.NORTH);
 
-		// Actual result
-		toyRobotService.left();
+        robot.setLocation(location);
 
-		// Assertion
-		assertEquals(1, robot.getLocation().getXCor());
-		assertEquals(2, robot.getLocation().getYCor());
-		assertEquals(Facing.WEST, robot.getLocation().getFacing());
-	}
+        // Actual result
+        toyRobotService.left();
 
-	@Test
-	public void shouldBeAbleToThrowErrorIfRobotDoesNotHaveLocation() {
-		// Assertion
-		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-			toyRobotService.left();
-		});
+        // Assertion
+        assertEquals(1, robot.getLocation().getXCor());
+        assertEquals(2, robot.getLocation().getYCor());
+        assertEquals(Facing.WEST, robot.getLocation().getFacing());
+    }
 
-		assertEquals(ERROR_MSG_NO_LOCATION, exception.getMessage());
-	}
+    @Test
+    public void shouldBeAbleToThrowErrorIfRobotDoesNotHaveLocation() {
+        // Assertion
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            toyRobotService.left();
+        });
+
+        assertEquals(ERROR_MSG_NO_LOCATION, exception.getMessage());
+    }
+
+    @Test
+    public void shouldBeAbleToFaceRobotNorthIfRobotIsFacingEastOnAPICall() {
+        toyRobotService.place("1", "2", "EAST");
+
+        webTestClient.post().uri("/api/toyrobot/left").exchange().expectStatus().isOk().expectBody(String.class)
+                .value(result -> {
+                    // Assertion
+                    assertEquals(1, robot.getLocation().getXCor());
+                    assertEquals(2, robot.getLocation().getYCor());
+                    assertEquals(Facing.NORTH, robot.getLocation().getFacing());
+                });
+    }
+
+    @Test
+    public void shouldBeAbleToFaceRobotEastIfRobotIsFacingSouthOnAPICall() {
+        toyRobotService.place("1", "2", "SOUTH");
+
+        webTestClient.post().uri("/api/toyrobot/left").exchange().expectStatus().isOk().expectBody(String.class)
+                .value(result -> {
+                    // Assertion
+                    assertEquals(1, robot.getLocation().getXCor());
+                    assertEquals(2, robot.getLocation().getYCor());
+                    assertEquals(Facing.EAST, robot.getLocation().getFacing());
+                });
+    }
+
+    @Test
+    public void shouldBeAbleToFaceRobotSouthIfRobotIsFacingWestOnAPICall() {
+        toyRobotService.place("1", "2", "WEST");
+
+        webTestClient.post().uri("/api/toyrobot/left").exchange().expectStatus().isOk().expectBody(String.class)
+                .value(result -> {
+                    // Assertion
+                    assertEquals(1, robot.getLocation().getXCor());
+                    assertEquals(2, robot.getLocation().getYCor());
+                    assertEquals(Facing.SOUTH, robot.getLocation().getFacing());
+                });
+    }
+
+    @Test
+    public void shouldBeAbleToFaceRobotWestIfRobotIsFacingNorthOnAPICall() {
+        toyRobotService.place("1", "2", "NORTH");
+
+        webTestClient.post().uri("/api/toyrobot/left").exchange().expectStatus().isOk().expectBody(String.class)
+                .value(result -> {
+                    // Assertion
+                    assertEquals(1, robot.getLocation().getXCor());
+                    assertEquals(2, robot.getLocation().getYCor());
+                    assertEquals(Facing.WEST, robot.getLocation().getFacing());
+                });
+    }
+
+    @Test
+    public void shouldBeAbleToThrowErrorIfRobotDoesNotHaveLocationOnAPICall() {
+        webTestClient.post().uri("/api/toyrobot/left").exchange().expectStatus().isBadRequest().expectBody(String.class)
+                .value(result -> {
+                    // Assertion
+                    assertEquals(ERROR_MSG_NO_LOCATION, result);
+                });
+    }
 }
